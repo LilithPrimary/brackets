077f3010585375cf55c6d727e72fe3b471f23660
Denna <lilithprimary@gmail.com>
2022-01-19 11:06:33 +0300

    feat: it works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = 
+function check(str, bracketsConfig) {
+  const pair = Object.fromEntries(bracketsConfig);
+  const openBrack = Object.keys(pair);
+  const simBrack = bracketsConfig.reduce((acc, el) => {
+    if (el[0]===el[1])
+      acc.push(el[0])
+    return acc;
+  }, [])
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (simBrack.includes(str[i])) {
+      if (str[i] === stack[stack.length-1]) {
+        stack.pop()
+      } else {
+        stack.push(str[i])
+      }
+    } else if (openBrack.includes(str[i])) {
+      stack.push(str[i])
+    } else if (str[i] === pair[stack[stack.length-1]]){
+        stack.pop()
+    } else {
+      stack.push(str[i]);
+      break};
+  }
+  return !stack.length;
 }
